1cbe712ed62511a60bb7972f13ca2c8d6a170b46
cryptomagnum <jorge_vankov@hotmail.com>
2024-05-16 16:33:44 +0200

    Genero funcion en repositorio para generar JSON de proyectos

--- src/Repository/ProyectRepository.php
+++ src/Repository/ProyectRepository.php
@@ -16,28 +16,7 @@ class ProyectRepository extends ServiceEntityRepository
         parent::__construct($registry, Proyect::class);
     }
 
-    //    /**
-    //     * @return Proyect[] Returns an array of Proyect objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('p.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Proyect
-    //    {
-    //        return $this->createQueryBuilder('p')
-    //            ->andWhere('p.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    public function getTasksJSON()
+    {
+    }
 }
